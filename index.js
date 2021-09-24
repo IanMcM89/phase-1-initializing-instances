6c5b8d99cf546e880d57de7b8f96233b4e667901
Ian <ianmcm.89@gmail.com>
2021-09-24 12:30:50 -0400

    commit tree 7061675ee231e1786386da6cf317b4f0356789a0

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your code here
+class Breakfast{
+    constructor(food, drink){
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+class Lunch{
+    constructor(salad, soup, drink){
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+class Dinner{
+    constructor(salad, soup, entree, dessert){
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this._dessert = dessert;
+    }
+}
+
+let breakfast = new Breakfast('Eggs', 'Coffee');
+
+let lunch = new Lunch('House', 'Chicken Noodle', 'Water');
+
+let dinner = new Dinner('Ceasar', 'Tomato', 'Chicken', 'Ice Cream');
+
+console.log(breakfast);
+console.log(lunch);
+console.log(dinner);
